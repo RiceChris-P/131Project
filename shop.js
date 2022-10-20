9476339f7ca8741371ecda468749a3e7bc90fb8b
Cameron <camharris831@gmail.com>
2022-10-19 18:55:27 -0700

    More suggested item and other ui changes

--- shop.js
+++ shop.js
@@ -1,6 +1,6 @@
 
 function createSuggestedItemNode() {
-    document.getElementById("suggestedItemContainer").innerHTML += ('<div class="suggestedItem"> <img src="banana.png" alt="banana" class="productImage"> <button class="cartButton">Add to cart</button></div>' )
+    document.getElementById("suggestedItemContainer").innerHTML += ('<div class="suggestedItem"> <img src="itemImages/banana.png" alt="banana" class="productImage"> <p class="itemName">Bananas</p> <button class="cartButton">Add to cart</button></div>' )
 }
 
 window.onload = function() {
