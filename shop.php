0c6ed8490d0064d14fdff2e35f22604480bd0726
Cameron <camharris831@gmail.com>
2022-10-26 19:35:14 -0700

    Connected SI to sql db, included sql db export

---
+++ shop.php
@@ -0,0 +1,54 @@
+<?php
+//Connect to local database
+$conn = mysqli_connect("localhost","root", "","stock");
+// Checking for connections
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+// SQL query to select data from database
+$sql = " SELECT * FROM items";
+$results= mysqli_query($conn,$sql);
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html>
+	<link rel="stylesheet" href="navstyle.css">
+	<div class="header">
+        <!--Site Header-->
+		<ul>
+			<img class="logo" src="logo-transparent.png" alt="">
+			<li><a href="login.php">Sign In</a></li>
+			<li><a href="navbar.html">Sign Up</a></li>
+			<li><a href="shop.php">Shop</a></li>
+			<li><a href="aboutus.html">About Us</a></li>
+			<li><a href="index.html">Home</a></li>
+		</ul>
+	</div>
+
+    <!--Body for suggested items-->
+	<body class="shopBody">
+		<h1 id="suggestedItemsTitle">Suggested Items:</h1>
+		<div id="suggestedItemContainer">
+			<?php
+				//Loop through sql data to display
+				while($rows=$results->fetch_assoc())
+				{
+			?>
+
+			<div class="suggestedItem"> 
+				<!--Displays each item from database-->
+				<img src="itemImages/<?php echo $rows['Image'];?>" alt="banana" class="productImage"> 
+				<p class="itemName"><?php echo $rows['Name'];?></p> 
+				<p class="itemPrice">Price: $<?php echo $rows['Price'];?> / ea</p>
+				<button class="cartButton">Add to cart</button>
+			</div>
+			
+			<?php
+				}
+			?>
+		</div>
+	</body>
+</html>
+
