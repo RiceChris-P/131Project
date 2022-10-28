70c206d921128e99634d2bd29e1044212b248eda
jordanlkfung <115450835+jordanlkfung@users.noreply.github.com>
2022-10-27 18:05:00 -0700

    fixed some links

--- signup.php
+++ signup.php
@@ -71,7 +71,7 @@
             <img class="logo" src="logo-transparent.png" alt="">
             <li><a href="login.php">Sign In</a></li>
             <li><a href="signup.php">Sign Up</a></li>
-            <li><a href="shop.html">Shop</a></li>
+            <li><a href="shop.php">Shop</a></li>
             <li><a href="aboutus.html">About Us</a></li>
             <li><a href="index.html">Home</a></li>
         </ul>
