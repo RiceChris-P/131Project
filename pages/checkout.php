c8b608c250e7716ae7d01470454d9586523a54e3
RESIDGE <k.boc@yahoo.com>
2022-11-08 01:20:39 -0800

    Added all checkout info form inputs

--- pages/checkout.php
+++ pages/checkout.php
@@ -15,41 +15,49 @@
 
     <body>
         <div>
-
-            <?php if(isset($_SESSION['login']) && $_SESSION['login'] == true) { ?>
-                <p>Dev Note: Logged Out</p>
-                <h1>Contact Information</h1>
-                <form action="">
-                    
-                    <input type="text" name="address" class="" placeholder="Street Address"><br>
-                    <input type="text" name="aptsuiteunit" class="" placeholder="Apt, suite, rtc. (optional)"><br>
-                    <input type="text" name="city" class="" placeholder="City"><br>
-                    <input type="text" name="state" class="" placeholder="State"><br>
-                    <input type="text" name="zip" class="" placeholder="ZIP"><br>
-
-                </form>
-            <?php } else { ?>
-                <form action="">
+            <div class="checkOutInfo">
+                <?php if(isset($_SESSION['login']) && $_SESSION['login'] == true) { ?>
                     <p>Dev Note: Logged Out</p>
-                    <h2>Contact Information</h2>
+                    <h1>Contact Information</h1>
+                    <form action="">
+                        
+                        <input type="text" name="address" class="" placeholder="Street Address"><br>
+                        <input type="text" name="aptsuiteunit" class="" placeholder="Apt, suite, rtc. (optional)"><br>
+                        <input type="text" name="city" class="" placeholder="City"><br>
+                        <input type="text" name="state" class="" placeholder="State"><br>
+                        <input type="text" name="zip" class="" placeholder="ZIP"><br>
 
-                    <input type="text" name="firstName" class="" placeholder="First Name"><br>
-                    <input type="text" name="lastName" class="" placeholder="Last Name"><br>
-                    <input type="text" name="phone" class="" placeholder="Phone Number"><br>
-                    <input type="text" name="email" class="" placeholder="Email Address"><br>
+                    </form>
+                <?php } else { ?>
+                    <form action="">
+                        <p>Dev Note: Logged Out</p>
+                        <h2>Contact Information</h2>
 
-                    <h2>Delivery Information</h2>
-                    <input type="text" name="address" class="" placeholder="Street Address"><br>
-                    <input type="text" name="aptsuiteunit" class="" placeholder="Apt, suite, etc. (optional)"><br>
-                    <input type="text" name="city" class="" placeholder="City"><br>
-                    <input type="text" name="state" class="" placeholder="State"><br>
-                    <input type="text" name="zip" class="" placeholder="ZIP"><br>
-                    <button type="submit"> </button>
-                </form>
-            <?php } ?> 
+                        <input type="text" name="firstName" class="" placeholder="First Name">
+                        <input type="text" name="lastName" class="" placeholder="Last Name"><br>
+                        <input type="text" name="email" class="" placeholder="Email Address"><br>
+                        <input type="text" name="phone" class="" placeholder="Phone Number">
 
-            <!-- <button type="button" onclick="showCheckOut()">Guest Checkout</button> -->
+                        <h2>Delivery Information</h2>
+                        <input type="text" name="address" class="" placeholder="Street Address"><br>
+                        <input type="text" name="aptsuiteunit" class="" placeholder="Apt, suite, etc. (optional)"><br>
+                        <input type="text" name="state" class="" placeholder="State">
+                        <input type="text" name="city" class="" placeholder="City">
+                        <input type="text" name="zip" class="" placeholder="ZIP"><br>
+                        
+                        <h2>Payment Information</h2>
+                        <input type="text" name="cardname" placeholder="Name On Card"><br>
+                        <input type="text" name="cardnumber" placeholder="Card Number">
+                        <input type="text" name="cardexpiration" placeholder="Exp MM/YY">
+                        <input type="text" name="cardcvv" placeholder="Enter CVV"><br>
+                        <button type="submit" name="checkoutsubmit">Submit Payment</button>
+                    </form>
+                <?php } ?> 
+            </div>
 
+            <div class="cartInCheckOut">
+                <h2>Cart</h2>
+            </div>
         </div>
     </body>
 </html>
