5fb76fc9606cb451d0a3c567978774a141afe8ff
JoseMonroyVillalobos <53064903+JoseMonroyVillalobos@users.noreply.github.com>
2022-11-10 17:43:59 -0800

    Update cart.php
    Test cart I was working on with custom database need to update to new data base Kevin made

--- fileswithbugs/cart.php
+++ fileswithbugs/cart.php
@@ -1,81 +1,81 @@
 <?php
+//include navbar
+
 //Connect to local database
-$conn = mysqli_connect("localhost","root", "","stock");
+$conn = mysqli_connect("localhost","root", "","cmpe131");
+
 // Checking for connections
 if (!$conn) {
-	die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
+//This gets email where the login status is true
+$sql = " SELECT * FROM accounts WHERE loginStatus = 1";
+$roq= mysqli_query($conn,$sql);
+$rows=$roq->fetch_assoc();
+$email= $rows['email'];
 
-// SQL query to select data from database
-$sql = " SELECT * FROM items";
+//This selects the rows of the email that is logged in
+$sql = " SELECT * FROM cart where email='$email'";
 $results= mysqli_query($conn,$sql);
 $conn->close();
 ?>
 
 <!DOCTYPE html>
 <html>
-	<link rel="stylesheet" href="navstyle.css">
-	<!--Site Header-->
-	<div class="header">
-		<ul>
-      <img class="logo" src="assets/logo-transparent.png" alt="">
-		  <li><a href="cart.php">Cart</a></li>
-			<li><a href="login.php">Sign In</a></li>
-			<li><a href="signup.php">Sign Up</a></li>
-			<li><a href="shop.php">Shop</a></li>
-			<li><a href="aboutus.html">About Us</a></li>
-			<li><a href="index.html">Home</a></li>
-    </ul>
+<head>
+    <title>Shopping Cart UI</title>
+    <link rel="stylesheet" type="text/css" href="../style/cartpage.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,900" rel="stylesheet">
+</head>
+<body>
+<div class="CartContainer">
+    <div class="Header">
+        <h3 class="Heading">Shopping Cart</h3>
+        <h5 class="Action">Remove all</h5>
+    </div>
+    <?php
+    //Loop through sql data to display
+    while($rows=$results->fetch_assoc())
+    {
+    ?>
+    <div class="Cart-Items">
+        <div class="image-box">
+            <img src="../itemImages/<?php echo $rows['Image'];?>"style={{ height="80" }} />
+        </div>
+        <div class="about">
+            <h1 class="title"><?php echo $rows['Name'];?></h1>
+            <h3 class="subtitle"><?php echo $rows['Weight'];?> lb</h3>
 
-    <link rel="stylesheet" href="cart.css">
-      <body>
-       <div class=”Cart-Container”></div>
-      </body>
-      <body>
-     <div class=”Cart-Container”></div>
-    </body>
+        </div>
+        <div class="counter">
+            <div class="btn">-</div>
+            <div class="count"><?php echo $rows['quantity'];?></div>
+            <div class="btn">+</div>
+        </div>
+        <div class="prices">
+            <div class="amount">$<?php echo number_format($rows['Price'], 2, ".", ",");?></div>
+            <div class="save"><u>Save for later</u></div>
+            <div class="remove"><u>Remove</u></div>
+        </div>
+    </div>
+        <hr>
 
-    <div class=”Header”>
-     <h3 class=”Heading”>Shopping Cart</h3>
-     <h5 class=”Action”>Remove all</h5>
-     </div>
+        <?php
+    }
+    ?>
 
-    <div class=”Cart-Items”>
-     <div class=”image-box”>
-     <img src=”images/apple.png” style={{ height=”120px” }} />
-     </div>
-     <div class=”about”>
-     <h1 class=”title”>Apple Juice</h1>
-     <h3 class=”subtitle”>250ml</h3>
-     <img src=”images/veg.png” style={{ height=”30px” }}/>
-     </div>
-     <div class=”counter”></div>
-     <div class=”prices”></div>
-     </div>
+    <div class="checkout">
+        <div class="total">
+            <div>
+                <div class="Subtotal">Sub-Total</div>
+                <div class="items">2 items</div>
+            </div>
+            <div class="total-amount">$6.18</div>
+        </div>
+        <a href="checkout.php">
+        <button class="button">Checkout</button></a>
 
-    <div class=”counter”>
-     <div class=”btn”>+</div>
-     <div class=”count”>2</div>
-     <div class=”btn”>-</div>
-     </div>
-
-
-    <div class=”prices”>
-     <div class=”amount”>$2.99</div>
-     <div class=”save”><u>Save for later</u></div>
-     <div class=”remove”><u>Remove</u></div>
-     </div>
-
-    <hr>
-     <div class=”checkout”>
-     <div class=”total”>
-     <div>
-     <div class=”Subtotal”>Sub-Total</div>
-     <div class=”items”>2 items</div>
-     </div>
-     <div class=”total-amount”>$6.18</div>
-     </div>
-     <button class=”button”>Checkout</button>
-     </div>
-
+    </div>
+</div>
+</body>
 </html>
