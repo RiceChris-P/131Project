e70dc09b01222d2719b0bc6a99f731563497180a
RiceChris-P <92416015+RiceChris-P@users.noreply.github.com>
2022-11-10 19:22:17 -0800

    About Us page implemented
    A basic about us page was implemented, listing the developers who worked on it and a brief description of what the website is and why it exists

--- pages/aboutus.php
+++ pages/aboutus.php
@@ -5,9 +5,9 @@
   
 	<head>
     <title>About Us</title>
-		<link rel="stylesheet" href="../style/aboutus.css">
-		<link rel="stylesheet" href="../style/navbar.css">
-		<link rel="stylesheet" href="../style/cart.css">
+		<link rel="stylesheet" type="text/css" href="../style/aboutus.css">
+		<link rel="stylesheet" type="text/css" href="../style/navbar.css">
+		<link rel="stylesheet" type="text/css" href="../style/cart.css">
 	</head>
 
 	<div class="cart" id="cart" style="visibility: hidden">
@@ -61,9 +61,51 @@
 		}
 
 	</script> 
-
-  <body class="staticBody">
-  </body>
+	<style>
+		.abth{
+			font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+			text-align: center;
+			padding-top: 2%;
+			font-size: 275%;
+		}
+		.abtDevs{
+			font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+			font-size: 190%;
+			text-align: center;
+			margin: auto;
+		}
+		.abtText{
+			font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+			font-size: 150%;
+			text-align: center;
+		}
+	</style>
+     <body>
+	 	<h1 class="abth"><b>Developers</b></h1>
+		<br>
+		<h1 class="abtDevs">Kevin Boc</b1>
+		<br>
+		<h1 class="abtDevs">Jordan Fung</b1>
+		<br>
+		<h1 class="abtDevs">Zachary Menes</b1>
+		<br>
+		<h1 class="abtDevs">Sharanya Udupa</b1>
+		<br>
+		<h1 class="abtDevs">Cameron Harris</b1>
+		<br>
+		<h1 class="abtDevs">Jose Monroy Villalobos</b1>
+		<br>
+		<h1 class="abtDevs">Christopher Park</b1>
+		<br>
+		<br>
+		<br>
+		<br>
+		<h1 class="abtText">This webpage is an online sales platform created for a downtown San Jose grocery</b1>
+		<h1 class="abtText">store that wanted to implement a delivery service. Created by an interdisciplinary</b1>
+		<h1 class="abtText">team of engineers at San Jose State University, everything from the webpage itself</b1>
+		<h1 class="abtText">to the databases used to track customer accounts, carts, and the items being sold</b1>
+		<h1 class="abtText">by the retailer was made from scratch.</b1>
+     </body>
 
 </html>
 
