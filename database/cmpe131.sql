71fed39ae8affd949f2640803e68beb07b585256
Cameron Harris <camharris831@gmail.com>
2022-11-29 18:12:43 -0800

    capitalized an s

--- database/cmpe131.sql
+++ database/cmpe131.sql
@@ -106,7 +106,7 @@ INSERT INTO `items` (`Name`, `Price`, `Weight`, `Image`, `Type`) VALUES
 ('Lobster', 62.99, 2, 'lobster.png', 'seafood'),
 ('Oyster', .75, .25, 'oyster.png', 'seafood'),
 ('Pound of Shrimp', 9.99, 1, 'shrimp.png', 'seafood'),
-('seabass', 14.99, 2, 'seabass.png', 'seafood'),
+('Seabass', 14.99, 2, 'seabass.png', 'seafood'),
 ('Scallop', 1.00, .35, 'scallop.png', 'seafood'),
 ('Butter', 5.99, 1, 'butter.png', 'Dairy');
 -- --------------------------------------------------------
