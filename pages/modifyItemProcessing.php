cc68b27666316d4ca21170046baecba9f8a1e09c
jordanlkfung <115450835+jordanlkfung@users.noreply.github.com>
2022-12-01 00:28:56 -0800

    should now be able to modify items

--- pages/modifyItemProcessing.php
+++ pages/modifyItemProcessing.php
@@ -1,28 +1,29 @@
 <?php
     if (isset($_POST['submit'])) {
+        $conn = mysqli_connect("localhost", "root", "", "cmpe131");
+        if(!$conn){
+            die("Connection failed: " . mysqli_connect_error());
+        }
         $oName=$_POST['oldItemName'];
         $itemName=$_POST['itemName'];
         $price=$_POST['price'];
         $weight=$_POST['weight'];
         $type=$_POST['itemType'];
         $inventory=$_POST['numOfItems'];
-        if(isset($_POST['file'])){
+        $len=$_FILES["file"]["name"];
+        if(strlen($len>3)){
             $targetDir = "../itemImages";
             $fileName = basename($_FILES["file"]["name"]);
             $targetFilePath = "$targetDir/$fileName";
-            $conn = mysqli_connect("localhost", "root", "", "cmpe131");
-            if(!$conn){
-                die("Connection failed: " . mysqli_connect_error());
-            }
             if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
-                $sql="UPDATE items Name='$itemName' Price='$price' Weight='$weight'
-                Type='$type' Stock='$inventory' Image='".$fileName."' WHERE Name='$oName'";
+                $sql="UPDATE items SET Name='$itemName', Price='$price', Weight='$weight',
+                Type='$type', Stock='$inventory', Image='".$fileName."' WHERE Name='$oName'";
                 mysqli_query($conn, $sql);
             }
         }
         else{
-            $sql="UPDATE items Name='$itemName' Price='$price' Weight='$weight'
-                Type='$type' Stock='$inventory' WHERE Name='$oName'";
+            $sql="UPDATE items SET Name='$itemName', Price='$price', Weight='$weight',
+                Type='$type', Stock='$inventory' WHERE Name='$oName'";
             mysqli_query($conn,$sql);
         }
     }
