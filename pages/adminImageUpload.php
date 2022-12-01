360fc9d2b98ff2ff37074d4287c70d246d2e2f54
jordanlkfung <115450835+jordanlkfung@users.noreply.github.com>
2022-11-30 20:24:56 -0800

    adding items to database should work

--- pages/adminImageUpload.php
+++ pages/adminImageUpload.php
@@ -3,7 +3,15 @@
     <input type="text" name="itemName"required>
     <input type="text" name="price" required>
     <input type="text" name="weight" required>
-    <input type="text" name="type" required>
+    <select name="itemType" >
+    <option value="dairy">Dairy</option>
+    <option value="seafood">Seafood</option>
+    <option value="meat">Meat</option>
+    <option value="vegetable">Vegetable</option>
+    <option value="fruit">Fruit</option>
+    </select>
+    <input tpye="text" name="itemType" required>
+    <input type="text" name="numOfItems" required>
     Select Image File to Upload:
     <input type="file" name="file">
     <input type="submit" name="submit" value="Upload">
--- pages/fileUpload.php
+++ pages/fileUpload.php
@@ -1,19 +1,25 @@
 <?php
     if (isset($_POST['submit'])) {
-        $targetDir = "desktop/";
+        $targetDir = "../itemImages";
         $fileName = basename($_FILES["file"]["name"]);
+        $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
         $itemName=$_POST['itemName'];
         $price=$_POST['price'];
         $weight=$_POST['weight'];
-        $type=$_POST['type'];
-        $targetFilePath = $targetDir . $fileName;
-        $conn = mysqli_connect("localhost", "root", "", "cmpe131"); 
-        if(!$conn){
-            die("Connection failed: " . mysqli_connect_error());
-        }
-        $sql="INSERT INTO items VALUES ('$itemName','$price','$weight','".$fileName."','$type')";
-        $results =mysqli_query($conn, $sql);       
-    }
+        $type=$_POST['itemType'];
+        $inventory=$_POST['numOfItems'];
+        $targetFilePath = "$targetDir/$fileName";
 
+        if($fileType=='png'){
+            $conn = mysqli_connect("localhost", "root", "", "cmpe131");
+            if(!$conn){
+                die("Connection failed: " . mysqli_connect_error());
+            }
+            if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
+                $sql="INSERT INTO items VALUES ('$itemName','$price','$weight','".$fileName."','$type','$inventory')";
+                mysqli_query($conn, $sql);
+            }
+        }
+    }
 ?>
 
