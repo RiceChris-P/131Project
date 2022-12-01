3091a7d58fc64e942b1506bd4d56c647c6cc5c7f
jordanlkfung <115450835+jordanlkfung@users.noreply.github.com>
2022-11-30 21:51:34 -0800

    addItemToDB fix1

--- pages/addItemToDatabase.php
+++ pages/addItemToDatabase.php
@@ -2,23 +2,22 @@
     if (isset($_POST['submit'])) {
         $targetDir = "../itemImages";
         $fileName = basename($_FILES["file"]["name"]);
-        $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+        $targetFilePath = "$targetDir/$fileName";
         $itemName=$_POST['itemName'];
         $price=$_POST['price'];
         $weight=$_POST['weight'];
         $type=$_POST['itemType'];
         $inventory=$_POST['numOfItems'];
-        $targetFilePath = "$targetDir/$fileName";
-
-        if($fileType=='png'){
-            $conn = mysqli_connect("localhost", "root", "", "cmpe131");
-            if(!$conn){
-                die("Connection failed: " . mysqli_connect_error());
-            }
-            if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
-                $sql="INSERT INTO items VALUES ('$itemName','$price','$weight','".$fileName."','$type','$inventory')";
-                mysqli_query($conn, $sql);
-            }
+        $conn = mysqli_connect("localhost", "root", "", "cmpe131");
+        if(!$conn){
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        if(move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)){
+            $sql="INSERT INTO items VALUES ('$itemName','$price','$weight','".$fileName."','$type','$inventory')";
+            mysqli_query($conn, $sql);
+        }
+        else{
+            echo "error";
         }
     }
 ?>
