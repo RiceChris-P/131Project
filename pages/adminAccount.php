605abfb9311cb650e6aebd91b1ecdd29268b6ea3
jordanlkfung <115450835+jordanlkfung@users.noreply.github.com>
2022-12-01 18:44:29 -0800

    quick fix

--- pages/adminAccount.php
+++ pages/adminAccount.php
@@ -37,27 +37,27 @@
     <form method="post" action="adminAccountProcessingPage.php">
         
         <label class= "userLabel" for="0">First Name</label><br>
-        <input class="userField" type="text" name="firstName" class="" id="0" placeholder="First Name"><br>
+        <input class="userField" type="text" name="firstName" class="" id="0" placeholder="First Name" required><br>
         
         <br>
         <label class= "userLabel"for="1">Last Name</label><br>
-        <input  class="userField" type="text" name="lastName" class="" id="1" placeholder="Last Name"><br>
+        <input  class="userField" type="text" name="lastName" class="" id="1" placeholder="Last Name" required><br>
         
         <br>
         <label class= "userLabel"for="2">Email</label><br>
-        <input  class="userField" type="text" name="email" class="" id="2"placeholder="Email Address"><br>
+        <input  class="userField" type="text" name="email" class="" id="2"placeholder="Email Address" required><br>
         <br>
 
         <label class= "userLabel"for="3">Password</label><br>
-        <input class="userField"  type="text" name="password" id="3"><br>
+        <input class="userField"  type="text" name="password" id="3" required><br>
         <br>
 
         <label class= "userLabel"for="4">Phone</label><br>
-        <input  class="userField" type="text" minlength="10" maxlength="10" name="phone" class="" id="4" placeholder="Phone Number" onkeypress="return isNumberKey(event)"><br>
+        <input  class="userField" type="text" minlength="10" maxlength="10" name="phone" class="" id="4" placeholder="Phone Number" onkeypress="return isNumberKey(event)" required><br>
         <br>
 
         <label class= "userLabel"for="5">Address</label><br>
-        <input  class="userField" type="text" name="address" class="" id="5" placeholder="Street Address"><br>
+        <input  class="userField" type="text" name="address" class="" id="5" placeholder="Street Address" required><br>
         <br>
 
         <label class= "userLabel"for="6">Apt Suit Unit</label><br>
@@ -65,31 +65,31 @@
         <br>
 
         <label class= "userLabel"for="7">State</label><br>
-        <input  class="userField" type="text" name="state" id="7" class="" placeholder="State"><br>
+        <input  class="userField" type="text" name="state" id="7" class="" placeholder="State" required><br>
         <br>
 
         <label class= "userLabel"for="8">City</label><br>
-        <input  class="userField" type="text" name="city" id="8" class="" placeholder="City"><br>
+        <input  class="userField" type="text" name="city" id="8" class="" placeholder="City" required><br>
         <br>
 
         <label class= "userLabel"for="9">Zip</label><br>
-        <input  class="userField" type="text" name="zip" minlength="5" maxlength="5" id="9" class="" placeholder="ZIP" onkeypress="return isNumberKey(event)"><br>
+        <input  class="userField" type="text" name="zip" minlength="5" maxlength="5" id="9" class="" placeholder="ZIP" onkeypress="return isNumberKey(event)" required><br>
         <br>
 
         <label class= "userLabel"for="10">Card Name</label><br>
-        <input  class="userField" type="text" name="cardname" id="10" placeholder="Name On Card"><br>
+        <input  class="userField" type="text" name="cardname" id="10" placeholder="Name On Card" required><br>
         <br>
 
         <label class= "userLabel"for="11">Card Number</label><br>
-        <input  class="userField" type="text" minlength="15" maxlength="16" name="cardnumber" id="11" placeholder="Card Number" onkeypress="return isNumberKey(event)"><br>
+        <input  class="userField" type="text" minlength="15" maxlength="16" name="cardnumber" id="11" placeholder="Card Number" onkeypress="return isNumberKey(event)" required><br>
         <br>
 
         <label class= "userLabel"for="12">Expiration Date</label><br>
-        <input  class="userField" type="text" name="cardexpiration" id="12" placeholder="Exp MM/YY"><br>
+        <input  class="userField" type="text" name="cardexpiration" id="12" placeholder="Exp MM/YY" required><br>
         <br>
 
         <label class= "userLabel"for="13">CVV</label><br>
-        <input  class="userField" minlength="3" maxlength="4" type="text" name="cardcvv" id="13" placeholder="Enter CVV" onkeypress="return isNumberKey(event)"><br>
+        <input  class="userField" minlength="3" maxlength="4" type="text" name="cardcvv" id="13" placeholder="Enter CVV" onkeypress="return isNumberKey(event)" required><br>
         <br>
 
         <input type="hidden" name="origSelectedEmail" value="<?php echo $inputtedEmail?>">
@@ -116,7 +116,7 @@
             return false;
         }
         else{
-            return true; 
+            return true||cardExpValidation(); 
         }
     }
     function isNumberKey(evt){
@@ -125,4 +125,12 @@
             return false;
         return true;
     }
+    function cardExpValidation() {
+    const cardexp = document.getElementById(12).value;
+    if(cardexp.length != 5) {
+        alert("Card expiration date must be in format MM/YY (e.g., 01/23).");
+        return false;
+    }
+    return false;
+}
 </script>
